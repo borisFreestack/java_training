43fbf678eeef513955af9f06c40fd2dca58c83d2
Boris Smets <boris.smets@freestack.rocks>
2021-11-23 18:23:39 +0100

    added some commons line code in Initializer.java

--- src/main/java/com/freestack/persistence/Initializer.java
+++ src/main/java/com/freestack/persistence/Initializer.java
@@ -1,16 +1,31 @@
 package com.freestack.persistence;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.transaction.Transactional;
 
 public class Initializer {
 
-    private final static String PERSISTANCE_UNIT_NAME = "myPostGreSqlEntityManager";
+    private static final String PERSISTANCE_UNIT_NAME = "myPostGreSqlEntityManager";
 
     public static void main(String[] args) {
-        EntityManagerFactory emf =
-                Persistence.createEntityManagerFactory(PERSISTANCE_UNIT_NAME);
+        EntityManagerFactory entityManagerFactory =
+            Persistence.createEntityManagerFactory(PERSISTANCE_UNIT_NAME);
+        try {
+            EntityManager entityManager = entityManagerFactory.createEntityManager();
+            entityManager.getTransaction().begin();
+
+
+            //your code here
+
+
+
+            entityManager.getTransaction().commit();
+            entityManager.close();
+        } finally {
+            entityManagerFactory.close();
+        }
+
 
     }
 
